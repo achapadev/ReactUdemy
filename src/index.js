707b9d9faa121f653e283c4ece1ebf99cbb87257
achapadev <83032999+achapadev@users.noreply.github.com>
2021-04-22 14:21:42 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+
+ReactDom.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.querySelector("#root")
+);
